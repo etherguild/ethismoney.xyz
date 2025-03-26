b5c63f09df9873c29ec04f6a58f6f16812d22759
Jason McPheron <jason.mcpheron@gmail.com>
2025-03-25 21:38:52 -0700

    Update Discord link

--- lib/urls.ts
+++ lib/urls.ts
@@ -4,7 +4,7 @@ export const EthHomepageURLs = {
   eth_holders: "https://api.growthepie.xyz/v1/eim/eth_holders.json",
 };
 
-export const DiscordURL = "https://discord.gg/yyH8NjJkkM";
+export const DiscordURL = "https://discord.gg/dAM6jwm9Ww";
 export const TwitterURL = "https://x.com/TheEtherGuild";
 export const FarcasterURL = "https://warpcast.com/etherguild";
 export const EmailURL = "mailto:info@etherguild.xyz";
